40e75091e7a412a713eb3922c4faad7baf73c47b
Oskars Bernhards <oskars.bernhards@gmail.com>
2022-08-09 13:50:27 +0300

    clean up

--- public/main.js
+++ public/main.js
@@ -1,34 +1,28 @@
-let dropDownWrap = document.querySelectorAll('.dd-wrap');
-
-
-document.addEventListener('click', function(event) {
-  resetToggle();
-
-  let menu = event.target;
-
-  if(!menu.classList.contains('fa-bars')) return;
-
-  let targetMenu = menu.closest('.wrapper').querySelector('.dd-wrap')
-
-  targetMenu.classList.toggle('active');
-})
+const dropDownWrap = document.querySelectorAll('.dd-wrap');
 
 // Reset opened status menus
 function resetToggle() {
-  dropDownWrap.forEach(el => {
+  dropDownWrap.forEach((el) => {
     el.className = 'dd-wrap';
-  })
+  });
 }
+
+document.addEventListener('click', (event) => {
+  resetToggle();
+
+  const menu = event.target;
+
+  if (!menu.classList.contains('fa-bars')) return;
+
+  const targetMenu = menu.closest('.wrapper').querySelector('.dd-wrap');
+
+  targetMenu.classList.toggle('active');
+});
 
 // Validate blank inputs
-const inputText = document.getElementById('inputText')
+const inputText = document.getElementById('inputText');
 inputText.addEventListener('change', () => {
-  if(inputText.value.trim().length === 0) {
+  if (inputText.value.trim().length === 0) {
     inputText.value = '';
   }
-})
-
-// Update status
-function updateStatus() {
-  document.querySelectorAll('.a-parent').submit();
-}
+});
--- server.js
+++ server.js
@@ -1,118 +1,110 @@
 // Declare variables
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
 const gameEntry = require('./models/game');
+require('dotenv').config();
 
+const app = express();
 
 // Set middleware
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(express.urlencoded({extended:true}))
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 // app.use(express.json())
-app.use(cors())
+app.use(cors());
 
-
-mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true}, () => {console.log('Connected to database..')}
-)
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true }, () => {
+  console.log('Connected to database..');
+});
 
 app.get('/', async (request, response) => {
-    try {
-        gameEntry.find({}, (err, entries) => {
-            response.render('index.ejs', {
-                gameList: entries
-            })
-        })
-    } catch (err) {
-        response.status(500).send({message: err.message})
-    }
-})
+  try {
+    gameEntry.find({}, (err, entries) => {
+      response.render('index.ejs', {
+        gameList: entries,
+      });
+    });
+  } catch (err) {
+    response.status(500).send({ message: err.message });
+  }
+});
 
 app.post('/', async (request, response) => {
-    const gameTitle = request.body.title
-    const newEntry = new gameEntry(
-        {
-            title: request.body.title,
-            status: request.body.status
-        }
-    )
-    try {    
-        gameEntry.find({title: gameTitle}, function(err, data){
-            if(err){
-                response.status(500).send({ message: err.message });
-            }
-            if(data.length > 0) {
-                console.log('already exist')
-                response.redirect('/')
-                return
-            }
-            newEntry.save();
-            console.log(newEntry)
-            response.redirect('/')
-        });       
-
-
-    } catch (err) {
+  const gameTitle = request.body.title;
+  const newEntry = new gameEntry({
+    title: request.body.title,
+    status: request.body.status,
+  });
+  try {
+    gameEntry.find({ title: gameTitle }, (err, data) => {
+      if (err) {
         response.status(500).send({ message: err.message });
-    }
-})
+      }
+      if (data.length > 0) {
+        console.log('already exist');
+        response.redirect('/');
+        return;
+      }
+      newEntry.save();
+      console.log(newEntry);
+      response.redirect('/');
+    });
+  } catch (err) {
+    response.status(500).send({ message: err.message });
+  }
+});
 
-app
-    .route('/update/:status/:id')
-    .get((request, response) => {
-        const id = request.params.id
-        const status = request.params.status
-        gameEntry.findByIdAndUpdate(
-            id,
-            {
-                status: status
-            },
-            err => {
-                if (err) return response.status(500).send(err)
-                response.redirect('/')
-            }
-        )
-    })
+app.route('/update/:status/:id').get((request, response) => {
+  const { id } = request.params;
+  const { status } = request.params;
+  gameEntry.findByIdAndUpdate(
+    id,
+    {
+      status: status,
+    },
+    (err) => {
+      if (err) return response.status(500).send(err);
+      response.redirect('/');
+    }
+  );
+});
 // Edit game title
 app
-    .route('/edit/:id')
-    .get((request, response) => {
-        const id = request.params.id
-        gameEntry.find({}, (error, entries) => {
-            response.render('edit.ejs', {
-                gameList: entries, idGame: id
-            })
-        })
-    })
-    .post((request, response) => {
-        const id = request.params.id
-        gameEntry.findByIdAndUpdate(
-            id,
-            {
-                title: request.body.title,
-                status: request.body.status
-            },
-            err => {
-                if (err) return response.status(500).send(error)
-                response.redirect('/')
-            }
-        )
-    })
+  .route('/edit/:id')
+  .get((request, response) => {
+    const { id } = request.params;
+    gameEntry.find({}, (error, entries) => {
+      response.render('edit.ejs', {
+        gameList: entries,
+        idGame: id,
+      });
+    });
+  })
+  .post((request, response) => {
+    const { id } = request.params;
+    gameEntry.findByIdAndUpdate(
+      id,
+      {
+        title: request.body.title,
+        status: request.body.status,
+      },
+      (err) => {
+        if (err) return response.status(500).send(err);
+        response.redirect('/');
+      }
+    );
+  });
 
 // Delete game entry
-app
-    .route('/remove/:id')
-    .get((request, response) => {
-        const id = request.params.id
-        gameEntry.findByIdAndRemove(id, err => {
-            if (err) return response.status(500).send(err)
-            response.redirect('/')
-        })
-    })
-
+app.route('/remove/:id').get((request, response) => {
+  const { id } = request.params;
+  gameEntry.findByIdAndRemove(id, (err) => {
+    if (err) return response.status(500).send(err);
+    response.redirect('/');
+  });
+});
 
 app.listen(process.env.PORT || PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`)
-})
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
